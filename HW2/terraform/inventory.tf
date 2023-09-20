30b0b57b0c04fe7fafdfdf9c24cd671ba3f64c81
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2023-09-20 22:18:13 +0400

    On branch HW2  Your branch is up to date with 'origin/HW2'.  Changes to be committed: 	modified:   ../../HW2/ansible/postgres_install.yml 	renamed:    ../../HW2/ansible/start_db_postgresql_to_vm2.yml -> ../../HW2/ansible/start_db_postgresql_to_pg2.yml 	renamed:    ../../HW2/ansible/stop_db_postgresql_vm1.yml -> ../../HW2/ansible/stop_db_postgresql_pg1.yml 	modified:   ../../HW2/ansible/vars.yml.example 	modified:   ../../HW2/terraform/infra_up.sh 	modified:   ../../HW2/terraform/inventory.tf 	modified:   ../../HW2/terraform/main.tf 	modified:   ../../README.md

--- HW2/terraform/inventory.tf
+++ HW2/terraform/inventory.tf
@@ -8,16 +8,3 @@ resource "local_file" "hosts_cfg" {
   filename = "../ansible/inventory"
 }
 
-# data  "template_file" "inv-yc" {
-#     template = "${file("./templates/inventory.tpl")}"
-#     vars {
-#      ext_ip_vm1 = "${join("\n", yandex_compute_instance.pg-1.network_interface.0.nat_ip_address)}",
-#      ext_ip_vm2 = "${join("\n", yandex_compute_instance.pg-2.network_interface.0.nat_ip_address)}"
-# }
-    
-# }
-
-# resource "local_file" "k8s_file" {
-#   content  = "${data.template_file.inv-yc.rendered}"
-#   filename = "../ansible/inventory"
-# }

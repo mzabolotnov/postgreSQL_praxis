a1dbfce7368fdeeb3f25fa15a2765b0036b80f6f
Mikhail Zabolotnov <m.zabolotnov@gmail.com>
2023-09-20 14:06:04 +0400

    On branch HW1  Your branch is up to date with 'origin/HW1'.  Changes to be committed: 	new file:   ../../.gitignore 	new file:   ../ansible/.gitignore 	new file:   ../ansible/ansible.cfg 	new file:   ../ansible/inventory.example 	new file:   ../ansible/postgres_install.yml 	new file:   ../ansible/vars.yml.example 	new file:   .gitignore 	new file:   infra_up.sh 	new file:   main.tf 	new file:   output.tf 	new file:   terraform.tfvars.example 	new file:   variables.tf 	new file:   version.tf 	new file:   ../../HW2/ansible/.gitignore 	new file:   ../../HW2/ansible/ansible.cfg 	new file:   ../../HW2/ansible/inventory.example 	new file:   ../../HW2/ansible/mv_db_postgresql_vm1.yml 	new file:   ../../HW2/ansible/postgres_install.yml 	new file:   ../../HW2/ansible/start_db_postgresql_to_vm2.yml 	new file:   ../../HW2/ansible/stop_db_postgresql_vm1.yml 	new file:   ../../HW2/ansible/vars.yml.example 	new file:   ../../HW2/terraform/.gitignore 	new file:   ../../HW2/terraform/infra_up.sh 	new file:   ../../HW2/terraform/inventory.tf 	new file:   ../../HW2/terraform/main.tf 	new file:   ../../HW2/terraform/output.tf 	new file:   ../../HW2/terraform/templates/inventory.tpl 	new file:   ../../HW2/terraform/terraform.tfvars.example 	new file:   ../../HW2/terraform/variables.tf 	new file:   ../../HW2/terraform/version.tf 	modified:   ../../README.md

---
+++ HW2/terraform/main.tf
@@ -0,0 +1,84 @@
+
+resource "yandex_compute_instance" "pg-1" {
+  name = "pg1"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  # secondary_disk {
+  #   disk_id = yandex_compute_disk.disk-2.id
+  #   device_name = "pgdata"
+  # }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
+
+resource "yandex_compute_instance" "pg-2" {
+  name = "pg2"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.image_id
+    }
+  }
+
+  # secondary_disk {
+  #   disk_id = yandex_compute_disk.disk-2.id
+  #   device_name = "pgdata"
+  # }
+
+
+  secondary_disk {
+    disk_id = yandex_compute_disk.disk-2.id
+    device_name = "pgdata"
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
+
+
+
+resource "yandex_compute_disk" "disk-2" {
+  name     = "disk-2"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "30"
+  image_id = "fd8bbad1kutvs49n1gal"
+  # image_id = "fd88fpagiunj098es21f"
+  
+
+
+  labels = {
+    environment = "postgres-data"
+  }
+}
+
+
+
---
+++ HW2/terraform/templates/inventory.tpl
@@ -0,0 +1,4 @@
+[vm-group-1]
+${ext_ip_vm1}
+[vm-group-2]
+${ext_ip_vm2}
